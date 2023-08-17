89479588a65af1ce89db34018f2fdee62c49d6d3
jeetm31 <jeetmajumder858@gmail.com>
2023-08-17 22:30:43 +0530

    InsideNav components

--- src/components/elements/InsideNav.tsx
+++ src/components/elements/InsideNav.tsx
@@ -1,8 +1,43 @@
 import * as React from "react";
-import { useState } from "react";
-import Select from "react-select";
+
+import SideNavElement from "./SideNavElement";
 const InsideNav: React.FunctionComponent = (props) => {
-  return <div className="h-24 w-full bg-red-500"></div>;
+  const OrderElements = [
+    {
+      name: "By Salesman",
+      href: "#",
+    },
+    {
+      name: "By Client",
+      href: "#",
+    },
+    {
+      name: "By Order Number",
+      href: "#",
+    },
+    {
+      name: "My Order",
+      href: "#",
+    },
+    {
+      name: "New Order (Mobile)",
+      href: "#",
+    },
+    {
+      name: "New Order (Web)",
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className="flex w-full justify-center pt-5">
+      <SideNavElement elementHeader="Order" element={OrderElements} />
+      <SideNavElement elementHeader="Stock" />
+      <SideNavElement elementHeader="Reports" />
+      <SideNavElement elementHeader="Uploads" />
+      <SideNavElement elementHeader="Master" />
+    </div>
+  );
 };
 
 export default InsideNav;

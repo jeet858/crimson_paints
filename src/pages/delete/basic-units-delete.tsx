5b25e809655f761bf6b751898d09de2b4d88724e
jeetm31 <jeetmajumder858@gmail.com>
2023-10-04 14:54:07 +0530

    Minor ui changes

--- src/pages/delete/basic-units-delete.tsx
+++ src/pages/delete/basic-units-delete.tsx
@@ -27,20 +27,20 @@ const BasicUnitsDelete: React.FunctionComponent = () => {
     <UserTemplate templateParams={templateParams}>
       <div className="flex h-full w-full items-center justify-center">
         <div className="flex h-1/3 w-1/3 flex-col rounded-xl bg-[#C4B0FF45]">
-          <p className="h-1/4 w-full items-center border-b-2 border-[#11009E] pl-4 text-lg font-semibold">
+          <p className="h-1/4 w-full items-center border-b-2 border-[#11009E] pl-4 pt-2 text-lg font-semibold">
             Basic Unit Details
           </p>
           <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
             Symbol
             <input
-              className="rounded-md border border-[#11009E] bg-[#C4B0FF45] outline-none"
+              className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
               value={editData.Symbol}
             />
           </div>
           <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
             Name
             <input
-              className="rounded-md border border-[#11009E] bg-[#C4B0FF45] outline-none"
+              className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
               value={editData.Name}
             />
           </div>

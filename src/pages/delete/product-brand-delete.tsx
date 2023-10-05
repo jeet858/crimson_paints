ebd01d49dd72fd6d895fe957c255a657a06043c9
Biswajit9999 <2003biswajit@gmail.com>
2023-10-05 22:43:53 +0530

    ..

--- src/pages/delete/product-brand-delete.tsx
+++ src/pages/delete/product-brand-delete.tsx
@@ -8,7 +8,7 @@ const get = async () => {
   return session;
 };
 
-const ProductCategoriesDelete: React.FunctionComponent = () => {
+const ProductBrandDelete: React.FunctionComponent = () => {
   const { data, status } = useSession();
   const templateParams = {
     title: "Admin",
@@ -20,31 +20,39 @@ const ProductCategoriesDelete: React.FunctionComponent = () => {
   const editData = {
     Symbol: "Gm",
     Name: "Gram",
+    Category:"Base"
   };
   const [confirmed, setConfirmed] = useState(false);
 
   return (
     <UserTemplate templateParams={templateParams}>
       <div className="flex h-full w-full items-center justify-center">
-        <div className="flex h-4/6 w-1/3 flex-col rounded-xl bg-[#C4B0FF45]">
-          <p className="h-1/4 w-full items-center border-b-2 border-[#11009E] pl-4 pt-2 text-lg font-semibold">
-          Product Categories Details
+        <div className="flex h-4/6 w-1/2 flex-col rounded-xl bg-[#C4B0FF45]">
+          <p className="h-1/6 w-full items-center border-b-2 border-[#11009E] pl-4 pt-2 text-lg font-semibold">
+            Product Brands Details
           </p>
-          <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
-          Name
+          <div className="flex h-1/6 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            Brand Name
             <input
               className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
               value={editData.Symbol}
             />
           </div>
-          <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
-            Code
+          <div className="flex h-1/6 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            HSN code
             <input
               className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
               value={editData.Name}
             />
           </div>
-          <div className="flex h-1/4 w-full justify-between self-end px-4">
+          <div className="flex h-1/6 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            Category
+            <input
+              className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
+              value={editData.Category}
+            />
+          </div>
+          <div className="flex h-1/6 w-full justify-between self-end px-4">
             <div className="flex h-fit items-center justify-center">
               <div
                 className="mr-2 flex h-4 w-4 items-center border-2 border-[#11009E] bg-[#C4B0FF45]"
@@ -56,10 +64,10 @@ const ProductCategoriesDelete: React.FunctionComponent = () => {
               </div>
               <p>I confirm the deletion</p>
             </div>
-            <button className="h-1/2 w-[25%] self-center rounded-md bg-[#07096E] font-semibold text-white">
+            <button className="h-1/2 w-1/4 self-center rounded-md bg-[#07096E] font-semibold text-white">
               Cancel
             </button>
-            <button className="h-1/2 w-[25%] self-center rounded-md bg-[#FF6E65] font-semibold text-white">
+            <button className="h-1/2 w-1/4 self-center rounded-md bg-[#FF6E65] font-semibold text-white">
               Delete
             </button>
           </div>
@@ -69,4 +77,4 @@ const ProductCategoriesDelete: React.FunctionComponent = () => {
   );
 };
 
-export default ProductCategoriesDelete;
+export default ProductBrandDelete;
--- src/pages/edit/product-brand-edit.tsx
+++ src/pages/edit/product-brand-edit.tsx
@@ -8,7 +8,7 @@ const get = async () => {
   return session;
 };
 
-const ProductCategoriesDelete: React.FunctionComponent = () => {
+const ProductBrandEdit: React.FunctionComponent = () => {
   const { data, status } = useSession();
   const templateParams = {
     title: "Admin",
@@ -20,32 +20,40 @@ const ProductCategoriesDelete: React.FunctionComponent = () => {
   const editData = {
     Symbol: "Gm",
     Name: "Gram",
+    Category:"Base"
   };
   const [confirmed, setConfirmed] = useState(false);
 
   return (
     <UserTemplate templateParams={templateParams}>
       <div className="flex h-full w-full items-center justify-center">
-        <div className="flex h-4/6 w-1/3 flex-col rounded-xl bg-[#C4B0FF45]">
-          <p className="h-1/4 w-full items-center border-b-2 border-[#11009E] pl-4 pt-2 text-lg font-semibold">
-          Product Categories Details
+        <div className="flex h-4/6 w-1/2 flex-col rounded-xl bg-[#C4B0FF45]">
+          <p className="h-1/6 w-full items-center border-b-2 border-[#11009E] pl-4 pt-2 text-lg font-semibold">
+            Product Brands Details
           </p>
-          <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
-          Name
+          <div className="flex h-1/6 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            Brand Name
             <input
               className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
               value={editData.Symbol}
             />
           </div>
-          <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
-            Code
+          <div className="flex h-1/6 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            HSN code
             <input
               className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
               value={editData.Name}
             />
           </div>
-          <div className="flex h-1/4 w-full justify-between self-end px-4">
-            <div className="flex h-fit items-center justify-center">
+          <div className="flex h-1/6 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            Category
+            <input
+              className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none"
+              value={editData.Category}
+            />
+          </div>
+          <div className="flex h-1/6 w-full justify-end space-x-4 px-4">
+            {/* <div className="flex h-fit items-center justify-center">
               <div
                 className="mr-2 flex h-4 w-4 items-center border-2 border-[#11009E] bg-[#C4B0FF45]"
                 onClick={() => {
@@ -55,12 +63,12 @@ const ProductCategoriesDelete: React.FunctionComponent = () => {
                 {confirmed ? <FaCheck className="h-4 w-4" /> : null}
               </div>
               <p>I confirm the deletion</p>
-            </div>
-            <button className="h-1/2 w-[25%] self-center rounded-md bg-[#07096E] font-semibold text-white">
+            </div> */}
+            <button className="h-1/2 w-1/6 self-center rounded-md bg-[#07096E] font-semibold text-white">
               Cancel
             </button>
-            <button className="h-1/2 w-[25%] self-center rounded-md bg-[#FF6E65] font-semibold text-white">
-              Delete
+            <button className="h-1/2 w-1/6 border border-[#11009E] self-center rounded-md bg-[#C4B0FF] font-semibold ">
+              Save
             </button>
           </div>
         </div>
@@ -69,4 +77,4 @@ const ProductCategoriesDelete: React.FunctionComponent = () => {
   );
 };
 
-export default ProductCategoriesDelete;
+export default ProductBrandEdit;

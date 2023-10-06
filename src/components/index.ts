e7d37dc65dab913d45e6bbe0e9852553cb141d29
Biswajit9999 <2003biswajit@gmail.com>
2023-10-06 21:19:00 +0530

    ..

--- src/components/index.ts
+++ src/components/index.ts
@@ -14,6 +14,23 @@ import OuterNav from "./layouts/OuterNav";
 import SideNav from "./layouts/SideNav";
 import DashboardTile from "./elements/DashboardTile";
 import LoginForm from "./forms/LoginForm";
+import CheckBox from "./elements/CheckBox";
+import AddUserForm from "./forms/AddUserForm";
+import BrandPackagingTable from "./tables/BrandPackagingTable";
+import BrandTable from "./tables/BrandTable";
+import ColorTable from "./tables/ColorTable";
+import GroupForProcingTable from "./tables/GroupForProcingTable";
+import MasterTable from "./tables/MasterTable";
+import OrderableUnitTable from "./tables/OrderableUnitTable";
+import OrderTable from "./tables/OrderTable";
+import ReportsTable from "./tables/ReportsTable";
+import StockTable from "./tables/StockTable";
+import TableComponentProxyAccess from "./tables/TableComponentProxyAccess";
+import UploadsTable from "./tables/UploadsTable";
+import UserAccessTable from "./tables/UserAccessTable";
+import LoginTemplate from "./template/LoginTemplate";
+
+
 
 export {
   LoginForm,
@@ -32,4 +49,19 @@ export {
   ComplexTypeTable,
   TableComponent,
   DashboardTile,
+  CheckBox,
+  AddUserForm,
+  BrandPackagingTable,
+  ColorTable,
+  BrandTable,
+  GroupForProcingTable,
+  MasterTable,
+  OrderableUnitTable,
+  OrderTable,
+  ReportsTable,
+  StockTable,
+  TableComponentProxyAccess,
+  UploadsTable,
+  UserAccessTable,
+  LoginTemplate,
 };

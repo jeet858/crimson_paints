483e2694afc66dfb1b0c10fb45bf539603f849fd
Biswajit9999 <2003biswajit@gmail.com>
2023-10-06 23:43:36 +0530

    Add pages

---
+++ src/pages/add/product-categories-add.tsx
@@ -0,0 +1,42 @@
+import { UserTemplate } from "@/components";
+import React from "react";
+import { useSession } from "next-auth/react";
+
+const ProductCategoriesAdd: React.FunctionComponent = () => {
+  const { data, status } = useSession();
+  const templateParams = {
+    title: "Admin",
+    userID: data?.user.id,
+    userImage: "user.jpg",
+    userType: "admin",
+  };
+  return (
+    <UserTemplate templateParams={templateParams}>
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex h-1/3 w-1/3 flex-col rounded-xl bg-[#C4B0FF45]">
+          <p className="h-1/4 w-full items-center border-b-2 border-[#11009E] pl-4 text-lg font-semibold">
+            Basic Unit Details
+          </p>
+          <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            Symbol
+            <input className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none" />
+          </div>
+          <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
+            Name
+            <input className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none" />
+          </div>
+          <div className="flex h-1/4 w-1/2 justify-between self-end px-4">
+            <button className="h-1/2 w-[40%] self-center rounded-md bg-[#07096E] font-semibold text-white">
+              Cancel
+            </button>
+            <button className="h-1/2 w-[40%] self-center rounded-md bg-[#C4B0FF] font-semibold">
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </UserTemplate>
+  );
+};
+
+export default ProductCategoriesAdd;

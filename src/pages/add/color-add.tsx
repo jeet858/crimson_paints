78cdb87dac32fd6fbe62c9b1447cdcb2d51ea50b
Biswajit9999 <2003biswajit@gmail.com>
2023-10-07 12:30:24 +0530

    ..

--- src/pages/add/color-add.tsx
+++ src/pages/add/color-add.tsx
@@ -14,15 +14,16 @@ const ColorAdd: React.FunctionComponent = () => {
     <UserTemplate templateParams={templateParams}>
       <div className="flex h-full w-full items-center justify-center">
         <div className="flex h-1/3 w-1/3 flex-col rounded-xl bg-[#C4B0FF45]">
+          Color Add
           <p className="h-1/4 w-full items-center border-b-2 border-[#11009E] pl-4 text-lg font-semibold">
-            Basic Unit Details
+            Color Details
           </p>
           <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
-            Symbol
+            Color Code
             <input className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none" />
           </div>
           <div className="flex h-1/4 items-center justify-between border-b-2 border-[#11009E] px-4 text-lg font-semibold">
-            Name
+            HTML Code
             <input className="rounded-md border border-[#11009E] bg-[#C4B0FF45] px-4 outline-none" />
           </div>
           <div className="flex h-1/4 w-1/2 justify-between self-end px-4">
